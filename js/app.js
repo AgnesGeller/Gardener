43553a6d94feaf03ad4a3efa03150d42e70c32ab
unknown <agnesgeller@cyberanalys.com>
2024-04-28 18:51:06 +0100

    navbar hiding after click menu item

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 AOS.init();
+document.addEventListener('DOMContentLoaded', function() {
+    var navbarToggler = document.querySelector('.navbar-toggler');
+    var navbarMenu = document.querySelector('#navbarNav');
+  
+    navbarMenu.addEventListener('click', function(event) {
+      if (event.target.classList.contains('nav-link')) {
+        if (navbarToggler.offsetParent !== null) {
+          event.stopPropagation(); // Stop the event from bubbling up
+          navbarToggler.click();
+        }
+      }
+    });
+  });
+  
