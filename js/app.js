93eb8fa2fb236c18d0c362af0e58ee97186afe4f
unknown <agnesgeller@cyberanalys.com>
2024-04-28 20:00:52 +0100

    Formspree added to contact

--- js/app.js
+++ js/app.js
@@ -13,3 +13,37 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
   
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var form = document.getElementById('myForm');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        // Construct the FormData object
+        var formData = new FormData(form);
+
+        // Send the form data using Fetch API
+        fetch(form.action, {
+            method: 'POST',
+            body: formData,
+            headers: {
+                'Accept': 'application/json'
+            }
+        }).then(response => {
+            if (response.ok) {
+                alert('Köszönjük! Az üzenetet sikeresen elküldtük.');
+                form.reset();
+            } else {
+                response.json().then(data => {
+                    if (Object.hasOwn(data, 'errors')) {
+                        alert(data["errors"].map(error => error["message"]).join(", "));
+                    } else {
+                        alert('Valami hiba történt és az üzenetet nem sikerült elküldeni.');
+                    }
+                });
+            }
+        }).catch(error => {
+            alert('Valami hiba történt és az üzenetet nem sikerült elküldeni.');
+        });
+    });
+});
