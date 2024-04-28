22992a3e279587a93cdf684f0d74adcc83f6d0e2
unknown <agnesgeller@cyberanalys.com>
2024-04-28 22:59:26 +0100

    dropdown menu added

--- js/app.js
+++ js/app.js
@@ -1,28 +1,56 @@
-AOS.init();
 document.addEventListener('DOMContentLoaded', function() {
+    // Initialize AOS
+    AOS.init();
+
+    // Navbar elements
     var navbarToggler = document.querySelector('.navbar-toggler');
     var navbarMenu = document.querySelector('#navbarNav');
-  
-    navbarMenu.addEventListener('click', function(event) {
-      if (event.target.classList.contains('nav-link')) {
-        if (navbarToggler.offsetParent !== null) {
-          event.stopPropagation(); // Stop the event from bubbling up
-          navbarToggler.click();
-        }
-      }
-    });
-  });
-  
+    var submenus = document.querySelectorAll('.dropdown-submenu');
 
-  document.addEventListener('DOMContentLoaded', function() {
+    // Toggle submenus on click
+    navbarMenu.addEventListener('click', function(event) {
+        var submenuTrigger = event.target.closest('.dropdown-submenu .dropdown-toggle');
+        if (submenuTrigger) {
+            event.preventDefault();
+            var submenu = submenuTrigger.nextElementSibling;
+            submenu.classList.toggle('show');
+        }
+    });
+
+    // Close the navbar when a nav-link is clicked on mobile
+    navbarMenu.addEventListener('click', function(event) {
+        if (event.target.classList.contains('nav-link')) {
+            if (navbarToggler.offsetParent !== null) {
+                navbarToggler.click();
+            }
+        }
+    });
+
+    // Close all submenus when toggler is clicked
+    navbarToggler.addEventListener('click', function() {
+        submenus.forEach(function(submenu) {
+            if (submenu.classList.contains('show')) {
+                submenu.classList.remove('show');
+            }
+        });
+    });
+
+    // Close submenus when clicking outside on mobile
+    document.addEventListener('touchstart', function(event) {
+        if (!navbarMenu.contains(event.target)) {
+            submenus.forEach(function(submenu) {
+                if (submenu.classList.contains('show')) {
+                    submenu.classList.remove('show');
+                }
+            });
+        }
+    });
+
+    // Form submission
     var form = document.getElementById('myForm');
     form.addEventListener('submit', function(event) {
         event.preventDefault();
-
-        // Construct the FormData object
         var formData = new FormData(form);
-
-        // Send the form data using Fetch API
         fetch(form.action, {
             method: 'POST',
             body: formData,
